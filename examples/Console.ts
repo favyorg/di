74a8fa564192fc423e9cc3e8207e7ad07a8abbd2
Alex <muzo0@yandex.ru>
2022-07-20 23:20:19 +0300

    update examples

--- examples/Console.ts
+++
@@ -1,9 +0,0 @@
-import { Module, Live } from '../src/index';
-
-export const Console = Module('Console', () => ({
-  log(text: string) {
-    console.log(text);
-  },
-}));
-
-export type ConsoleLive = Live<typeof Console>;
